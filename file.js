f5e1f5ab083e2a3496b64b0c9b182faaf81e8ce1
andy770921 <andy770921@yahoo.com.tw>
2019-04-14 01:31:57 +0800

    Replace for-loop with forEach()

--- file.js
+++ file.js
@@ -20,21 +20,21 @@ function getList(path, callback) {
 getList(originalPath, function(list){
 // 2. 將檔案複製到指定資料夾
 	console.log("file-name had been saved in array: "+list);
-
-	for (i=0; i<list.length; i++){
-		var readable = fs.createReadStream(originalPath+'/'+list[i]); // 建立可讀(Readable)串流
-		var writable = fs.createWriteStream(finalPath+'/'+list[i]); // 建立可寫(Writable)串流
+	list.forEach(function(element) {
+		var readable = fs.createReadStream(originalPath+'/'+element); // 建立可讀(Readable)串流
+		var writable = fs.createWriteStream(finalPath+'/'+element); // 建立可寫(Writable)串流
 
 		// 所有來自 readable 的資料，都會被寫入到 '/logs/TEST/'+arr[i]' 檔中
 		readable.pipe(writable);
-		console.log('file copied:'+list[i]); 
-		};
+		console.log('file copied:'+element); 
+		}
+	);
 	console.log('files copied completed'); 
+	
 });
-
 // 設定 1 毫秒後，解除 pipe() 的串流，由於 1 毫秒過短，所以應該會複製不了，設500ms。
-	setTimeout(function(){
-		readable.unpipe(writable);
-		writable.end();
-	}, 500);
+setTimeout(function(){
+	readable.unpipe(writable);
+	writable.end();
+}, 500);
 
