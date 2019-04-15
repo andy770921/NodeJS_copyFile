608cc74287e958b0ecee82ede3b36babf6555150
andy770921 <andy770921@yahoo.com.tw>
2019-04-15 16:29:48 +0800

    delete dummy array

--- file.js
+++ file.js
@@ -7,13 +7,12 @@ var fs = require('fs'); // 引入檔案系統模組
 // 1. 先取得資料夾下檔案目錄，再將檔名存入自己宣告的陣列
 function getList(path, callback) {
 	fs.readdir(path, function (err,files) {
-		var arr = []; 
 		if(err){
 		  console.log(err);
 		 }
 		 console.log(files);
-		 arr = files;
-		 callback(arr);
+		 // files陣列建立完成
+		 callback(files);
 	});
 };
 
@@ -24,7 +23,7 @@ getList(originalPath, function(list){
 		var readable = fs.createReadStream(originalPath+'/'+element); // 建立可讀(Readable)串流
 		var writable = fs.createWriteStream(finalPath+'/'+element); // 建立可寫(Writable)串流
 
-		// 所有來自 readable 的資料，都會被寫入到 '/logs/TEST/'+arr[i]' 檔中
+		// 所有來自 readable 的資料，都會被寫入到 '/logs/TEST/'+files[element]' 檔中
 		readable.pipe(writable);
 		console.log('file copied:'+element); 
         
